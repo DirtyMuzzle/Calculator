4777b359f2dafc3bc4e5ec92113f704c770734aa
Flapes32 <114094178+Flapes32@users.noreply.github.com>
2022-12-07 14:05:25 +0300

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -1,12 +1,21 @@
 int sum(int a, int b) {
 	return a + b;
 }
-  
 int sub(int a, int b) {
 	return a - b;
 }
-
-
 int mult(int a, int b) {
 	return a * b;
-}  
+}
+int division(int a, int b) {
+	return a / b;
+}
+int mode(int a,int b) {
+	return a % b;
+}
+int UnaryPlus(int a) {
+	return +a;
+}
+int UnaryMinus(int a) {
+	return-a;
+}
