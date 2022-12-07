ef46681260a0415740332820f1dae4ec2dbd7126
Flapes32 <114094178+Flapes32@users.noreply.github.com>
2022-12-07 14:40:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ int main() {
 	cout << "1.sum" << endl;
 	cout << "2.sub" << endl;
 	cout << "3.mult" << endl;
+	std::cout << "4.division";
+	std::cout << "5.mode";
+	std::cout << "6.UnaryPlus";
+	std::cout << "7.UnaryMinus";
 	cout << "Choose function:" << endl;
 	cin >> func;
 	switch (func) {
@@ -19,6 +23,18 @@ int main() {
 		cout << sub(a, b) << endl;
 	case 3:
 		cout << mult(a, b) << endl;
+			case 4:
+		std::cout << division(a, b);
+		break;
+	case 5:
+	    std::cout << mode(a, b);
+		break;
+	case 6:
+		std::cout << UnaryPlus(a);
+		break;
+	case 7:
+		std::cout << UnaryMinus(a);
+		break;
 	default:
 		cout << "Wrong function!!!" << endl;
 	}
