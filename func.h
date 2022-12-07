dcdeef314fb2c2fb32cea8f69f46c6589312ba1d
DirtyMuzzle <120017941+DirtyMuzzle@users.noreply.github.com>
2022-12-07 14:15:03 +0300

    Update func.h

--- func.h
+++ func.h
@@ -1,1 +1,8 @@
-int sum(int a, int b)
+#pragma once
+int sum(int a, int b);
+int sub(int a, int b);
+int mult(int a, int b);
+int division(int a, int b);
+int mode(int a, int b);
+int UnaryPlus(int a);
+int UnaryMinus(int a);
