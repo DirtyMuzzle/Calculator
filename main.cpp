bd0088eccbee4a2253cc743bba101087661cf162
DirtyMuzzle <120017941+DirtyMuzzle@users.noreply.github.com>
2022-12-07 14:35:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include "func.h"
 using namespace std;
-int main()
-{
- int a, b;
+int main() {
+	int a, b,func;
 	cin >> a >> b;
-   cout << sum(a, b) << endl;
+	cout << "List of functions:" << endl;
+	cout << "1.sum" << endl;
+	cout << "2.sub" << endl;
+	cout << "3.mult" << endl;
+	cout << "Choose function:" << endl;
+	cin >> func;
+	switch (func) {
+	case 1:
+		cout << sum(a, b) << endl;
+		break;
+	case 2:
+		cout << sub(a, b) << endl;
+	case 3:
+		cout << mult(a, b) << endl;
+	default:
+		cout << "Wrong function!!!" << endl;
+	}
 }
