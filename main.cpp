a1d6c003a8c22d166bdcde3d36cba9a408dd8fb3
DirtyMuzzle <120017941+DirtyMuzzle@users.noreply.github.com>
2022-12-07 14:43:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
 #include "func.h"
-using namespace std;
 int main() {
 	int a, b,func;
-	cout << "Insert 2 numbers:" << endl;
-	cin >> a >> b;
-	cout << "List of functions:" << endl;
-	cout << "1.sum" << endl;
-	cout << "2.sub" << endl;
-	cout << "3.mult" << endl;
+	std::cout << "Insert 2 numbers:" << endl;
+	std::cin >> a >> b;
+	std::cout << "List of functions:" << endl;
+	std::cout << "1.sum" << endl;
+	std::cout << "2.sub" << endl;
+	std::cout << "3.mult" << endl;
 	std::cout << "4.division";
 	std::cout << "5.mode";
 	std::cout << "6.UnaryPlus";
 	std::cout << "7.UnaryMinus";
-	cout << "Choose function:" << endl;
-	cin >> func;
+	std::cout << "Choose function:" << endl;
+	std::cin >> func;
 	switch (func) {
 	case 1:
-		cout << sum(a, b) << endl;
+		std::cout << sum(a, b) << endl;
 		break;
 	case 2:
-		cout << sub(a, b) << endl;
+		std::cout << sub(a, b) << endl;
+		break;
 	case 3:
-		cout << mult(a, b) << endl;
-			case 4:
-		std::cout << division(a, b);
+		std::cout << mult(a, b) << endl;
+		break;
+	case 4:
+		std::cout << division(a, b) << endl;
 		break;
 	case 5:
-	    std::cout << mode(a, b);
+	        std::cout << mode(a, b) << endl;
 		break;
 	case 6:
-		std::cout << UnaryPlus(a);
+		std::cout << UnaryPlus(a) << endl;
 		break;
 	case 7:
-		std::cout << UnaryMinus(a);
+		std::cout << UnaryMinus(a) << endl;
 		break;
 	default:
-		cout << "Wrong function!!!" << endl;
+		std::cout << "Wrong function!!!" << endl;
 	}
 }
