421a1fc49318b580826ba8af6c06acca575b81dc
DirtyMuzzle <120017941+DirtyMuzzle@users.noreply.github.com>
2022-12-14 19:34:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,41 +2,42 @@
 #include "func.h"
 int main() {
 	int a, b,func;
-	std::cout << "Insert 2 numbers:" << endl;
+	std::cout << "Insert 2 numbers:" << std::endl;
 	std::cin >> a >> b;
-	std::cout << "List of functions:" << endl;
-	std::cout << "1.sum" << endl;
-	std::cout << "2.sub" << endl;
-	std::cout << "3.mult" << endl;
-	std::cout << "4.division";
-	std::cout << "5.mode";
-	std::cout << "6.UnaryPlus";
-	std::cout << "7.UnaryMinus";
-	std::cout << "Choose function:" << endl;
+	std::cout << "List of functions:" << std::endl;
+	std::cout << "1.sum" << std::endl;
+	std::cout << "2.sub" << std::endl;
+	std::cout << "3.mult" << std::endl;
+	std::cout << "4.division" << std::endl;
+	std::cout << "5.mode" << std::endl;
+	std::cout << "6.UnaryPlus" << std::endl;
+	std::cout << "7.UnaryMinus" << std::endl;
+	std::cout << "Choose function:" << std::endl;
 	std::cin >> func;
+	std::cout << "Result:" << std::endl;
 	switch (func) {
 	case 1:
-		std::cout << sum(a, b) << endl;
+		std::cout << sum(a, b) << std::endl;
 		break;
 	case 2:
-		std::cout << sub(a, b) << endl;
+		std::cout << sub(a, b) << std::endl;
 		break;
 	case 3:
-		std::cout << mult(a, b) << endl;
+		std::cout << mult(a, b) << std::endl;
 		break;
 	case 4:
-		std::cout << division(a, b) << endl;
+		std::cout << division(a, b) << std::endl;
 		break;
 	case 5:
-	        std::cout << mode(a, b) << endl;
+	        std::cout << mode(a, b) << std::endl;
 		break;
 	case 6:
-		std::cout << UnaryPlus(a) << endl;
+		std::cout << UnaryPlus(a) << std::endl;
 		break;
 	case 7:
-		std::cout << UnaryMinus(a) << endl;
+		std::cout << UnaryMinus(a) << std::endl;
 		break;
 	default:
-		std::cout << "Wrong function!!!" << endl;
+		std::cout << "Wrong function!!!" << std::endl;
 	}
 }
