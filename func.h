b7f98148eadd4057252485c0c26ada1d3de46710
Flapes32 <114094178+Flapes32@users.noreply.github.com>
2022-12-16 20:24:36 +0300

    Update func.h

--- func.h
+++ func.h
@@ -1,17 +1,62 @@
-#pragma once
-//standart
-int sum(int a, int b);
-int sub(int a, int b);
-int mult(int a, int b);
-int division(int a, int b);
-int mode(int a, int b);
-int UnaryPlus(int a);
-int UnaryMinus(int a);
+#include <cmath>
+int sum(int a, int b) {
+	return a + b;
+}
+int sub(int a, int b) {
+	return a - b;
+}
+int mult(int a, int b) {
+	return a * b;
+}
+int division(int a, int b) {
+	return a / b;
+}
+int mode(int a, int b) {
+	return a % b;
+}
+int UnaryPlus(int a) {
+	return +a;
+}
+int UnaryMinus(int a) {
+	return-a;
+}
 
 //programmer
-int bitwiseNot(int a);
-int bitwiseAnd(int a, int b);
-int bitwiseOr(int a, int b);
-int bitwiseExclusiveOr(int a, int b);
-int leftShift(int a, int b);
-int rightShift(int a, int b);
+
+int bitwiseNot(int a) {
+	return ~a;
+}
+int bitwiseAnd(int a, int b) {
+	return a & b;
+}
+int bitwiseOr(int a, int b) {
+	return a | b;
+}
+int bitwiseExclusiveOr(int a, int b) {
+	return a ^ b;
+}
+int leftShift(int a, int b) {
+	return a << b;
+}
+int rightShift(int a, int b) {
+	return a >> b;
+}
+bool negation(bool a) {
+	return !a;
+	}
+bool conjunction(bool a, bool b) {
+	return a && b;
+}
+bool disjunction(bool a, bool b) {
+	return a || b;
+}
+//Scientific
+int AbsoluteValue(int a) {
+	return abs(a);
+}
+double SquareRoot(double a) {
+	return sqrt(a);
+}
+double ReciprocalFunction(double a) {
+	return 1 / a;
+}
