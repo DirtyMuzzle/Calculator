566930c3fd49a1c033827f105add0c06c6101891
DirtyMuzzle <120017941+DirtyMuzzle@users.noreply.github.com>
2022-12-17 01:31:12 +0300

    Add files via upload

---
+++ standartfunc.h
@@ -0,0 +1,8 @@
+#pragma once
+int sum(int a, int b);
+int sub(int a, int b);
+int mult(int a, int b);
+int division(int a, int b);
+int mode(int a, int b);
+int UnaryPlus(int a);
+int UnaryMinus(int a);
