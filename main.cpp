d502ac822738de88c3132d46a756d3336def22a1
DirtyMuzzle <120017941+DirtyMuzzle@users.noreply.github.com>
2022-12-17 01:34:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
 #include "func.h"
+using namespace std;
 int main() {
-	int a, b,func;
-	std::cout << "Insert 2 numbers:" << std::endl;
-	std::cin >> a >> b;
-	std::cout << "List of functions:" << std::endl;
-	std::cout << "1.sum" << std::endl;
-	std::cout << "2.sub" << std::endl;
-	std::cout << "3.mult" << std::endl;
-	std::cout << "4.division" << std::endl;
-	std::cout << "5.mode" << std::endl;
-	std::cout << "6.UnaryPlus" << std::endl;
-	std::cout << "7.UnaryMinus" << std::endl;
-	std::cout << "Choose function:" << std::endl;
-	std::cin >> func;
-	std::cout << "Result:" << std::endl;
-	switch (func) {
+	int a, b, calctype, func;
+	cout << "Insert 2 numbers:" << endl;
+	cin >> a >> b;
+	cout << calculatortype();
+	cin >> calctype;
+	switch (calctype) {
 	case 1:
-		std::cout << sum(a, b) << std::endl;
+		cout << standartlist();
+		cin >> func;
+		cout << endl << "Result:" << endl;
+		cout << standartfunc(a, b, func);
 		break;
 	case 2:
-		std::cout << sub(a, b) << std::endl;
+		cout << "Will be added in the next update";
 		break;
 	case 3:
-		std::cout << mult(a, b) << std::endl;
-		break;
-	case 4:
-		std::cout << division(a, b) << std::endl;
-		break;
-	case 5:
-	        std::cout << mode(a, b) << std::endl;
-		break;
-	case 6:
-		std::cout << UnaryPlus(a) << std::endl;
-		break;
-	case 7:
-		std::cout << UnaryMinus(a) << std::endl;
+		cout << "Will be added in the next update";
 		break;
 	default:
-		std::cout << "Wrong function!!!" << std::endl;
+		std::cout << "Wrong type!!!" << endl;
 	}
 }
