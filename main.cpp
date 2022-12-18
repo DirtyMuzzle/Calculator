af54728b18ba265e8d6fad9a1ade50a257efa2c2
DirtyMuzzle <120017941+DirtyMuzzle@users.noreply.github.com>
2022-12-18 17:18:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,5 @@
 #include <iostream>
 #include "func.h"
-using namespace std;
 int main() {
-	int a, b, calctype, func;
-	cout << "Insert 2 numbers:" << endl;
-	cin >> a >> b;
-	cout << calculatortype();
-	cin >> calctype;
-	switch (calctype) {
-	case 1:
-		cout << standartlist();
-		cin >> func;
-		cout << endl << "Result:" << endl;
-		cout << standartfunc(a, b, func);
-		break;
-	case 2:
-		cout << "Will be added in the next update";
-		break;
-	case 3:
-		cout << "Will be added in the next update";
-		break;
-	default:
-		std::cout << "Wrong type!!!" << endl;
-	}
+	std::cout << calculator();
 }
