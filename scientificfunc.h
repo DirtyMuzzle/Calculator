0195411c6d23d4f4d3a50f28028159ad94322271
Flapes32 <114094178+Flapes32@users.noreply.github.com>
2022-12-30 18:59:11 +0300

    Update scientificfunc.h

--- scientificfunc.h
+++ scientificfunc.h
@@ -2,6 +2,20 @@
 int AbsoluteValue(int a);
 double SquareRoot(double a);
 double ReciprocalFunction(double a);
-int factorial(int a);
-double Degree(double a, int m);
+int Factorial(int a);
+double AToThePowerOfB(double a, int m);
 double Square(double a);
+double TenToThePowerOfB(int b);
+double log(double a, double b);
+double ln(double a);
+/*double timeanddate();*/
+double sinus(double a);
+double cos(double a);
+double Tg(double a);
+double Ctg(double a);
+double Asin(double a);
+double Acos(double a);
+double Atg(double a);
+double Actg(double a);
+double expToThePow(int a);
+int mod(int a, int b);
