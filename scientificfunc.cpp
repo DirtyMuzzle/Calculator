a045884ed55513b460546f8afce00b828229cff5
Flapes32 <114094178+Flapes32@users.noreply.github.com>
2022-12-30 15:41:37 +0300

    Update scientificfunc.cpp

--- scientificfunc.cpp
+++ scientificfunc.cpp
@@ -14,3 +14,31 @@ double SquareRoot(double a) {
 double ReciprocalFunction(double a) {
 	return 1 / a;
 }
+int factorial(int a) {
+	if (a < 0) {
+		return 0;
+	}
+	if (a == 0) {
+		return 1;
+	}
+	else {
+		return a * factorial(a - 1);
+	}
+}
+double square(double a) {
+	return a * a;
+}
+double degree(double a,int m) {
+	if (m == 0) {
+		return 0;
+	}
+	if (m == 1) {
+		return a;
+	}
+	else {
+		for (int i = 1; i < m; i++) {
+			a = a * a;
+		}
+		return a;
+	}
+}
